cb69495be87362cfdd44e8c3aecc7977d732c926
ra6yee <ra6yee@mail.ru>
2021-07-04 04:51:55 +0300

    работа с сериализацией

--- src/main/java/разное_тонкости/длина_числа.java
+++ src/main/java/разное_тонкости/длина_числа.java
@@ -2,6 +2,7 @@ package разное_тонкости;
 
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+
 public class длина_числа {//нахождение количества цифр в числе.
    int x;
 
@@ -13,4 +14,13 @@ class   завершение_программы{
 }
 class перенос_строки{
     String signature= System.lineSeparator()+" JavaRush © All rights reserved.";
+}
+
+class вызов_нестатика_из_статика{
+    // C c = new Solution().new C("ну Цэ типа");
+    // где Solution  это общий класс, а С его вложенный или внутренний клас
+    // имеется ввиду что майн назодится в солюшн
+  //  Solution56 solution = new Solution56();
+   // Solution56.B b = solution.new B("B2", "C33");
+
 }
